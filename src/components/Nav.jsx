518bfb461dbb90a9776929eba4d14fa32d2e1386
Nazrin K Faizal <nazrinkf@gmail.com>
2025-07-17 13:45:51 +0530

    Router

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <div>
-        <nav class="navbar navbar-expand-lg bg-info">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/"><h2>Student Internal</h2></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg bg-info">
+  <div className="container-fluid">
+    <Link className="navbar-brand" href="/"><h2>Student Internal</h2></Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="/">Calculate</a>
-        <a class="nav-link" href="#">Help</a>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <Link className="nav-link active" aria-current="page" href="/">Calculate</Link>
+        <Link className="nav-link" href="#">Help</Link>
         
       </div>
     </div>
